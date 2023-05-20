921c11ad0474450dab7133719e06ac5a99866730
CodeDeyya <78592159+CodeDeyya@users.noreply.github.com>
2023-05-20 22:54:03 +0530

    Fixes on drop down not showing with help of mentor

--- script.js
+++ script.js
@@ -1,40 +1,124 @@
-const input = document.querySelector('#fruit');
-const suggestions = document.querySelector('.suggestions ul');
+const input = document.querySelector("#fruit");
+const suggestions = document.querySelector("#suggest");
 
-const fruit = ['Apple', 'Apricot', 'Avocado', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+const fruit = [
+  "Apple",
+  "Apricot",
+  "Avocado",
+  "Banana",
+  "Bilberry",
+  "Blackberry",
+  "Blackcurrant",
+  "Blueberry",
+  "Boysenberry",
+  "Currant",
+  "Cherry",
+  "Coconut",
+  "Cranberry",
+  "Cucumber",
+  "Custard apple",
+  "Damson",
+  "Date",
+  "Dragonfruit",
+  "Durian",
+  "Elderberry",
+  "Feijoa",
+  "Fig",
+  "Gooseberry",
+  "Grape",
+  "Raisin",
+  "Grapefruit",
+  "Guava",
+  "Honeyberry",
+  "Huckleberry",
+  "Jabuticaba",
+  "Jackfruit",
+  "Jambul",
+  "Juniper berry",
+  "Kiwifruit",
+  "Kumquat",
+  "Lemon",
+  "Lime",
+  "Loquat",
+  "Longan",
+  "Lychee",
+  "Mango",
+  "Mangosteen",
+  "Marionberry",
+  "Melon",
+  "Cantaloupe",
+  "Honeydew",
+  "Watermelon",
+  "Miracle fruit",
+  "Mulberry",
+  "Nectarine",
+  "Nance",
+  "Olive",
+  "Orange",
+  "Clementine",
+  "Mandarine",
+  "Tangerine",
+  "Papaya",
+  "Passionfruit",
+  "Peach",
+  "Pear",
+  "Persimmon",
+  "Plantain",
+  "Plum",
+  "Pineapple",
+  "Pomegranate",
+  "Pomelo",
+  "Quince",
+  "Raspberry",
+  "Salmonberry",
+  "Rambutan",
+  "Redcurrant",
+  "Salak",
+  "Satsuma",
+  "Soursop",
+  "Star fruit",
+  "Strawberry",
+  "Tamarillo",
+  "Tamarind",
+  "Yuzu",
+];
 
 function search(str) {
-	let matches = [];
-	input = str.toLowerCase();
-	for (let i = 0; i < fruit.length; i++) {
-		if (fruit[i].toLowerCase().includes(input)) {
-			matches.push(fruit[i]);
-		}
-	}// TODO
-	return matches;
+  let matches = [];
+  if (!str) return matches;
+  let lowercaseInput = str.toLowerCase();
+  for (let i = 0; i < fruit.length; i++) {
+    if (fruit[i].toLowerCase().includes(lowercaseInput)) {
+      matches.push(fruit[i]);
+    }
+  } // TODO
+  return matches;
 }
 
 function searchHandler(e) {
-	const results = search(e.target.value);
-	showSuggestions(results, e.target.value);
-	// TODO
+  const results = search(e.target.value);
+  showSuggestions(results, e.target.value);
+  // TODO
 }
 
 function showSuggestions(results, inputVal) {
-	if (inputVal === '') {
-		suggestions.innerHTML = '';
-		return;
-	}
-	// TODO
+  suggestions.innerHTML = "";
+
+  for (let i = 0; i < results.length; i++) {
+    const fruit = results[i];
+    var strRegExp = new RegExp(inputVal, "gi");
+    const boldFruit = fruit.replace(strRegExp, "<b>$&</b>");
+    suggestions.innerHTML += `<li>${boldFruit}</li>`;
+  }
 }
 
 function useSuggestion(e) {
-	if (e.target.tagName === 'LI') {
-		input.value = e.target.textContent;
-		suggestions.innerHTML = '';
-	}
-	// TODO
+  if (e.target.tagName === "LI") {
+    input.value = e.target.textContent;
+    suggestions.innerHTML = "";
+  }
+  // TODO
 }
 
-input.addEventListener('keyup', searchHandler);
-suggestions.addEventListener('click', useSuggestion);
+input.addEventListener("keyup", searchHandler);
+suggestions.addEventListener("click", useSuggestion);
